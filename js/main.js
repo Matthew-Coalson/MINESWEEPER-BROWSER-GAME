deac3398af881b6bb5c38771ba8b570d86ac5235
Matthew Coalson <matthew.coalson@gmail.com>
2020-02-08 18:05:51 -0600

    adds timer framework and other various work in progress things

--- js/main.js
+++ js/main.js
@@ -2,29 +2,33 @@
 
 const boardState = {
     tiles: {}
-};
-boardState.tiles[0] = 0;
-console.log(boardState.tiles);
-boardState.tiles[0][state] = 'b';
-console.log(boardState.tiles[0].state);
-boardState.tiles.tile[0].adjacent = [];
-boardState.tiles.tile[0].adjacent.push('taco');
+}; 
+const revealState = {
+    tiles: {}
+}
+const timerEl = document.querySelector('h2');
+// timer();
 
-console.log(boardState);
-console.log(boardState.tiles[0]);
-boardState.tiles[1].adjacent.push('taco');
-boardState.tiles[1].adjacent.push('bell');
-console.log(boardstate.tiles.adjacent)
+// timer;
+
 
 /*----- app's state (variables) -----*/
 
+let boardLayout;
 let columnCount = 10;
 let rowCount = 8;
 let bombCount = 10;
+let revealedTiles = 0;
+let seconds = 0;
+let minutes = 0;
+let timeNow;
 /*----- cached element references -----*/
-makeTiledBoard(findBombAdjacent(getBombs(10, 8, 10)));
+boardLayout = makeTiledBoard(findBombAdjacent(getBombs(10, 8, 10)));
 /*----- event listeners -----*/
 /*----- functions -----*/
+function checkWinCon() {
+    return revealedTiles === columnCount * rowCount - bombCount;
+}
 
 function makeTiledBoard(board) {
     let y;
@@ -33,10 +37,12 @@ function makeTiledBoard(board) {
         y = i;
         for(let i = 0; i < board[y].length; i++) {
             boardState.tiles[count] = board[y][i];
+            revealState.tiles[count] = 'h';
             count += 1;
         }
     }
 }
+
 
 function findBombAdjacent(board) {
     let y;
@@ -104,72 +110,24 @@ function findBombAdjacent(board) {
     return board;
 }
 
-//needs fixing
-// function findBlankAdjacent(board) {
-//     let y;
-//     for (let i = 0; i < board.length; i++) {
-//         y = i;
-//         for(let i = 0; i < board[y].length; i++) {
-//             if (board[y][i] === 0) {
-//                 if (y + 1 < board.length && y - 1 >= 0 && i + 1 < board[y].length && i - 1 >= 0) {
-//                     if (board[y - 1][i - 1] !== 'b') board[y - 1][i - 1] += 1;
-//                     if (board[y - 1][i] !== 'b') board[y - 1][i] += 1;
-//                     if (board[y - 1][i + 1] !== 'b') board[y - 1][i + 1] += 1;
-//                     if (board[y][i - 1] !== 'b') board[y][i - 1] += 1;
-//                     if (board[y][i + 1] !== 'b') board[y][i + 1] += 1;
-//                     if (board[y + 1][i - 1] !== 'b') board[y + 1][i - 1] += 1;
-//                     if (board[y + 1][i] !== 'b') board[y + 1][i] += 1;
-//                     if (board[y + 1][i + 1] !== 'b') board[y + 1][i + 1] += 1;
-//                 } else if (y - 1 < 0) {
-//                     if (i - 1 < 0) {
-//                         if (board[y][i + 1] !== 'b') board[y][i + 1] += 1;
-//                         if (board[y + 1][i] !== 'b') board[y + 1][i] += 1;
-//                         if (board[y + 1][i + 1] !== 'b') board[y + 1][i + 1] += 1;
-//                     } else if (i + 1 > board[y].length - 1) {
-//                         if (board[y][i - 1] !== 'b') board[y][i - 1] += 1;
-//                         if (board[y + 1][i - 1] !== 'b') board[y + 1][i - 1] += 1;
-//                         if (board[y + 1][i] !== 'b') board[y + 1][i] += 1;
-//                     } else {
-//                         if (board[y][i - 1] !== 'b') board[y][i - 1] += 1;
-//                         if (board[y][i + 1] !== 'b') board[y][i + 1] += 1;
-//                         if (board[y + 1][i + 1] !== 'b') board[y + 1][i + 1] += 1;
-//                         if (board[y + 1][i] !== 'b') board[y + 1][i] += 1;
-//                         if (board[y + 1][i - 1] !== 'b') board[y + 1][i - 1] += 1;
-//                     }
-//                 } else if (y + 1 > board.length - 1) {
-//                     if (i - 1 < 0) {
-//                         if (board[y][i + 1] !== 'b') board[y][i + 1] += 1;
-//                         if (board[y - 1][i] !== 'b') board[y - 1][i] += 1;
-//                         if (board[y - 1][i + 1] !== 'b') board[y - 1][i + 1] += 1;
-//                     } else if (i + 1 > board[y].length - 1) {
-//                         if (board[y][i - 1] !== 'b') board[y][i - 1] += 1;
-//                         if (board[y - 1][i - 1] !== 'b') board[y - 1][i - 1] += 1;
-//                         if (board[y - 1][i] !== 'b') board[y - 1][i] += 1;
-//                     } else {
-//                         if (board[y][i - 1] !== 'b') board[y][i - 1] += 1;
-//                         if (board[y][i + 1] !== 'b') board[y][i + 1] += 1;
-//                         if (board[y - 1][i + 1] !== 'b') board[y - 1][i + 1] += 1;
-//                         if (board[y - 1][i] !== 'b') board[y - 1][i] += 1;
-//                         if (board[y - 1][i - 1] !== 'b') board[y - 1][i - 1] += 1;
-//                     }
-//                 } else if (i - 1 < 0) {
-//                     if (board[y - 1][i] !== 'b') board[y - 1][i] += 1;
-//                     if (board[y - 1][i + 1] !== 'b') board[y - 1][i + 1] += 1;
-//                     if (board[y][i + 1] !== 'b') board[y][i + 1] += 1;
-//                     if (board[y + 1][i + 1] !== 'b') board[y + 1][i + 1] += 1;
-//                     if (board[y + 1][i] !== 'b') board[y + 1][i] += 1;
-//                 } else if (i + 1 > board[y].length - 1) {
-//                     if (board[y - 1][i] !== 'b') board[y - 1][i] += 1;
-//                     if (board[y - 1][i - 1] !== 'b') board[y - 1][i - 1] += 1;
-//                     if (board[y][i - 1] !== 'b') board[y][i - 1] += 1;
-//                     if (board[y + 1][i - 1] !== 'b') board[y + 1][i - 1] += 1;
-//                     if (board[y + 1][i] !== 'b') board[y + 1][i] += 1;
-//                 }
-//             }
-//         }
-//     }
-//     return board;
-// }
+function checkBlank(index) {
+    if(boardLayout[index] !== 'b' && revealState[index] === 'h') {
+        const x = index % columnCount;        
+        const y = Math.floor( index / columnCount);
+        revealedTiles++;
+        reavealState[index] = 'r';
+        if(boardLayout[index] === 0) {
+            if(x > 0 && revealState[index - 1] === 'h') checkBlank(index - 1);
+            if(y > 0 && revealState[index - columnCount] === 'h') checkBlank(index - columnCount);
+            if(x > 0 && y > 0 && revealState[index - columnCount - 1] === 'h') checkBlank(index - columnCount - 1);
+            if(x > 0 && y < (rowCount - 1) && y < (rowCount - 1) && revealState[index + columnCount - 1] === 'h') checkBlank(index + columnCount - 1);
+            if(x < (columnCount - 1) && revealState[index + 1] === 'h') checkBlank(index + 1);
+            if(y < (rowCount - 1) && revealState[index + columnCount] === 'h') checkBlank(index + columnCount);
+            if(x < (columnCount - 1) && y < (rowCount - 1) && revealState[index + columnCount + 1] === 'h') checkBlank(index + columnCount + 1);
+            if(x < (columnCount - 1) && y > 0 && y < (rowCount - 1) && revealState[index - columnCount + 1] === 'h') checkBlank(index - columnCount + 1);
+        }
+    }
+}
 
 function getBombs(columns, rows, bombs) {
     let tempRowArr = new Array(columns).fill(0);
@@ -191,4 +149,29 @@ function getBombs(columns, rows, bombs) {
 
 function getRandomIdx(max) {
     return Math.floor(Math.random() * Math.floor(max));
+}
+
+function addTime() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+    }
+    
+    timerEl.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+    timer();
+}
+
+function timer() {
+    timeNow = setTimeout(addTime, 1000);
+}
+
+function stop() {
+    clearTimeout(timeNow);
+}
+
+function clear() {
+    timerEl.textContent = "00:00";
+    seconds = 0;
+    minutes = 0;
 }
